209a27932e06a4fbabb1d70f54f19402fe6f7edb
HeeSun You <sim2626@naver.com>
2021-12-20 18:29:00 +0900

    init

---
+++ pocketmark_backend/src/main/java/com/example/pocketmark/PocketmarkApplication.java
@@ -0,0 +1,13 @@
+package com.example.pocketmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketmarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketmarkApplication.class, args);
+	}
+
+}
---
+++ pocketmark_backend/src/test/java/com/example/pocketmark/PocketmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pocketmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketmarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
