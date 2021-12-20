4c09a6cff72cb68102a6e4902b3d0583fd7ccb87
HeeSun You <sim2626@naver.com>
2021-12-20 17:21:45 +0900

    Ping9Branch Test

--- pocketmark_frontend/src/components/pages/ErrorPage.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-export default function ErrorPage(){
-    return <div><h1>Error PAGE</h1></div>
-} 
--- pocketmark_frontend/src/components/pages/Example.jsx
+++
@@ -1,7 +0,0 @@
-export default function Example(){
-    return(
-        <div>
-            EXAMPLE page....
-        </div>
-    );
-}
