4b36c4262c7ebe38accd5f5094bb8d11250372cb
JyuKa <yoyksks3@gmail.com>
2021-12-20 19:03:33 +0900

    branch test3

--- pocketmark_backend/src/main/java/com/example/pocketmark/controller/ApiController.java
+++ pocketmark_backend/src/main/java/com/example/pocketmark/controller/ApiController.java
@@ -1,2 +1,7 @@
-# branch test
-# fdfdfdffdfd
+package com.example.pocketmark.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+}
