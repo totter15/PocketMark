eae29b5a6c93e880ca4f74e1169f0b635f08330f
HeeSun You <sim2626@naver.com>
2021-12-20 17:37:31 +0900

    from the scratch

--- pocketmark_backend/src/main/java/com/bookmarkmanager/bookmarkmanager/db/repository/BookmarkRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bookmarkmanager.bookmarkmanager.db.repository;
-
-import com.bookmarkmanager.bookmarkmanager.db.entity.Bookmark;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookmarkRepository extends JpaRepository<Bookmark,Long> {
-}
--- pocketmark_backend/src/main/java/com/bookmarkmanager/bookmarkmanager/db/repository/FolderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bookmarkmanager.bookmarkmanager.db.repository;
-
-import com.bookmarkmanager.bookmarkmanager.db.entity.Folder;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FolderRepository extends JpaRepository<Folder,Long> {
-}
