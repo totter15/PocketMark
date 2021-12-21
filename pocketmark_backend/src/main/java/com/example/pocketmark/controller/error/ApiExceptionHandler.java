5c22b898f27c63b10d1a33cd41f1ba16a4ff2cee
JyuKa <yoyksks3@gmail.com>
2021-12-21 23:16:57 +0900

    공통된 응답을 위한 ApiDataResponse 작성

---
+++ pocketmark_backend/src/main/java/com/example/pocketmark/controller/error/ApiExceptionHandler.java
@@ -0,0 +1,55 @@
+package com.example.pocketmark.controller.error;
+
+import com.example.pocketmark.constant.ErrorCode;
+import com.example.pocketmark.dto.ApiErrorResponse;
+import com.example.pocketmark.exception.GeneralException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.validation.ConstraintViolationException;
+
+@RestControllerAdvice(annotations = {RestController.class})
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<Object> validation(ConstraintViolationException e, WebRequest request) {
+        return handleExceptionInternal(e, ErrorCode.VALIDATION_ERROR, request);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Object> general(GeneralException e, WebRequest request) {
+        return handleExceptionInternal(e, e.getErrorCode(), request);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Object> exception(Exception e, WebRequest request) {
+        return handleExceptionInternal(e, ErrorCode.INTERNAL_ERROR, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return handleExceptionInternal(ex, ErrorCode.valueOf(status), headers, status, request);
+    }
+
+
+    private ResponseEntity<Object> handleExceptionInternal(Exception e, ErrorCode errorCode, WebRequest request) {
+        return handleExceptionInternal(e, errorCode, HttpHeaders.EMPTY, errorCode.getHttpStatus(), request);
+    }
+
+    private ResponseEntity<Object> handleExceptionInternal(Exception e, ErrorCode errorCode, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return super.handleExceptionInternal(
+                e,
+                ApiErrorResponse.of(false, errorCode.getCode(), errorCode.getMessage(e)),
+                headers,
+                status,
+                request
+        );
+    }
+
+}
