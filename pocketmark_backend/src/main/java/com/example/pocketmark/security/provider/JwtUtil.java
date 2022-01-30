c813433af8ec3f568ddf472f278495a23d4c12f3
HeeSun You <sim2626@naver.com>
2022-01-31 05:04:50 +0900

    Main 브랜치로 병합 전, devOnly Option RollBack

--- pocketmark_backend/src/main/java/com/example/pocketmark/security/provider/JwtUtil.java
+++ pocketmark_backend/src/main/java/com/example/pocketmark/security/provider/JwtUtil.java
@@ -23,9 +23,9 @@ public class JwtUtil {
 
     // deploy - 15분
     // developOnly - 6 hours
-    // public static long ACCESS_TOKEN_EXPIRATION_TIME = 900000L;
-    public static long ACCESS_TOKEN_EXPIRATION_TIME = Duration.ofMinutes(60*6).toMillis();
-//    public static long ACCESS_TOKEN_EXPIRATION_TIME = 15000L;
+    public static long ACCESS_TOKEN_EXPIRATION_TIME = 900000L;
+    // public static long ACCESS_TOKEN_EXPIRATION_TIME = Duration.ofMinutes(60*6).toMillis();
+
     // 24주
     public static long REFRESH_TOKEN_EXPIRATION_TIME = 604800000L;
 
