29d87f6ef50f823f6d60a4baf1d45223eac6d6da
천진아 <totter15@naver.com>
2023-08-18 21:40:18 +0900

    refactor : 코드 분할로 성능 향상

--- pocketmark_frontend/src/App.tsx
+++ pocketmark_frontend/src/App.tsx
@@ -1,28 +1,31 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import React from 'react';
-import Regist from './pages/Regist';
-import RegistOk from './pages/RegistOk';
-import Login from './pages/Login';
-import Main from './pages/Main';
+import React, { Suspense, lazy } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Provider } from 'react-redux';
 import { store } from '../src/slices/index';
+
+const Login = lazy(() => import('./pages/Login'));
+const Regist = lazy(() => import('./pages/Regist'));
+const RegistOk = lazy(() => import('./pages/RegistOk'));
+const Main = lazy(() => import('./pages/Main'));
 
 function App() {
 	const queryClient = new QueryClient();
 
 	return (
-		<Provider store={store}>
-			<QueryClientProvider client={queryClient}>
-				<Routes>
-					<Route path='/' element={<Login />} />
-					<Route path='/regist' element={<Regist />} />
-					<Route path='/registOk' element={<RegistOk />} />
-					<Route path='/main' element={<Main />} />
-				</Routes>
-			</QueryClientProvider>
-		</Provider>
+		<Suspense fallback={<div>로딩중...</div>}>
+			<Provider store={store}>
+				<QueryClientProvider client={queryClient}>
+					<Routes>
+						<Route path='/' element={<Login />} />
+						<Route path='/regist' element={<Regist />} />
+						<Route path='/registOk' element={<RegistOk />} />
+						<Route path='/main' element={<Main />} />
+					</Routes>
+				</QueryClientProvider>
+			</Provider>
+		</Suspense>
 	);
 }
 
